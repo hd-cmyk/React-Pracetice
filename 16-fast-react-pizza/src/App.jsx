98a9c25d5bbd788a3b3149bbb0e1c366e4b7dc47
hd-cmyk <h52294602@gmail.com>
2025-12-22 02:15:39 +0800

    16-fast-react-pizza build frame

---
+++ 16-fast-react-pizza/src/App.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import Cart from "./features/cart/Cart";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+    ],
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
