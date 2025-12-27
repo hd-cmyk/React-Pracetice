2702270ee71305c763cdad6864b92b9276ab7edc
hd-cmyk <h52294602@gmail.com>
2025-12-28 02:28:39 +0800

    start 17-the-wild-oasis

---
+++ 17-the-wild-oasis/src/App.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-md);
+  cursor: pointer;
+  margin: 20px;
+`;
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+export default App;
