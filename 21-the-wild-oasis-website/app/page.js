15e81dad820b0ba76e75f7c07efcc314442ccd5c
hd-cmyk <h52294602@gmail.com>
2026-01-14 19:40:04 +0800

    Finish 1 overview: 21-the-wild-oasis-website

--- 21-the-wild-oasis-website/app/page.js
+++ 21-the-wild-oasis-website/app/page.js
@@ -1,3 +1,10 @@
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>Hello Next!</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise</h1>
+      <a href="/cabins">Explore Luxury Cabins</a>
+    </div>
+  );
 }
